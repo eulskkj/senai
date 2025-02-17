ef65e1a5a5c9aa85da03341bf284d203e78d8dd7
eulskkj <pikapaybeludo@gmail.com>
2025-02-17 18:43:02 +0000

    salvando

---
+++ 2.tipagem_estatica.py
@@ -0,0 +1,15 @@
+# definição de tipos obrigatórias
+import os
+os.system("clear")
+nome: str # Equivalente ao tipo caracter
+idade: int # Equivalente ao tipo inteiro
+altura: float #Equivalente ao tipo real
+
+nome = "Marta"
+idade = 23
+altura = 1.65
+
+print(nome)
+print(idade)
+print(altura)
+
---
+++ 3.tipagem_dinamica.py
@@ -0,0 +1,12 @@
+# definição de tipos automática
+import os
+os.system("clear")
+
+
+nome = "Marta"
+idade = 23
+altura = 1.65
+
+print(nome)
+print(idade)
+print(altura)
