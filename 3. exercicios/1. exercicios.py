f0d4807b6a3b46db9f541a5cb564f155a87ec436
eulskkj <pikapaybeludo@gmail.com>
2025-02-19 17:40:29 +0000

    salvamento

---
+++ 3. exercicios/1. exercicios.py
@@ -0,0 +1,17 @@
+import os
+os.system("clear")
+
+#Elabore um algoritmo para solicitar ao usuário um valor e escrever a
+#mensagem: É MAIOR QUE 10! 
+#Se o valor lido for maior que 10, caso contrário escrever: NÃO É
+#MAIOR QUE 10!
+
+idade=10
+
+if idade <10:
+    print ("MENOR QUE DEZ")
+else:
+    print ("MAIOR QUE DEZ")
+print("== FIM ==")
+
+
