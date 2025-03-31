6accadbaa2fa47267c657479d79016c2e95793c5
eulskkj <pikapaybeludo@gmail.com>
2025-03-31 14:10:27 -0300

    Update 6. atividade.py

--- vs_code2.1/continuação/6. atividade.py
+++ vs_code2.1/continuação/6. atividade.py
@@ -1,7 +1,28 @@
+#Foi feita uma pesquisa entre os habitantes de uma região.  Foram coletados os dados de idade, sexo (M/F) e salário. 
+#Faça um algoritmo que informe:  
+#a) a média de salário do grupo;
+#b) maior e menor idade do grupo;
+#c) quantidade de mulheres com salário a partir de R$ 5.000,00.
+#Crie um menu com três opções.
+#Código |   Descrição
+#1        |   Adicionar pessoa
+#2       |   Exibir resultados
+#3       |   Sair
+#O final da leitura de dados se dará com quando o usuário digitar o número código 3. 
+#Ao adicionar uma pessoa, deve-se limpar o terminal e apresentar o menu novamente.
+
+import os
+os.system(cls || clear)
+
 contador = 0
 soma = 0
 salario_feminino = 0
 idades = []
+
+
+
+
+
 
 while True:
     # Mostrando o menu
